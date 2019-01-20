1ffb21cd93553d98e045f16ca1ac805b0a8710f8
Matei Adriel <rafaeladriel11@gmail.com>
2019-01-20 12:53:57 +0200

    typescript(translucid): deprecated false
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/translucid/Translucid.js
+++ typescript/translucid/Translucid.js
@@ -19,7 +19,7 @@ class Translucid {
     bind(path = "/", filepath = "", text = false, classes = []) {
         this.app.get(path, async (req, res) => {
             if (!text) {
-                res.sendFile(`${__dirname}/../../${filepath}`);
+                console.error("Blocking reading acces s deprecated, and would be remove in the next version.");
             }
             else {
                 const readResults = await read_1.read(filepath);
