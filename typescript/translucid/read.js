f697f49b5e31a6250470f5d5da3b0950805b72cb
Matei Adriel <rafaeladriel11@gmail.com>
2019-01-20 12:45:23 +0200

    typescript(translucid): idk anymore
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/translucid/read.js
+++ typescript/translucid/read.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require('fs');
 function read(url) {
     return new Promise((resolve, reject) => {
-        fs.readFile(`${__dirname}/${url}`, "utf8", (err, data) => {
+        fs.readFile(`${__dirname}/../../${url}`, "utf8", (err, data) => {
             if (err)
                 reject(err);
             resolve(data);
