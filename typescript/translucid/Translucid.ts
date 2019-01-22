01b28c642b577b75d8274c202a54c372e947e8f1
Matei Adriel <rafaeladriel11@gmail.com>
2019-01-22 22:14:51 +0200

    typescript(translucid): i think i fixed it
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/translucid/Translucid.ts
+++ typescript/translucid/Translucid.ts
@@ -35,7 +35,7 @@ class Translucid {
             // console.log(`Binded room with name ${i} and path ${object[i].file} with classes ${classes}`)
         }
     }
-    bind(path:string = "/", filepath:string = "", classes:Array<string> = []):void{
+    bind(path:string = "/", filepath:string = "", classes:Array<string> = [],sendFiles:boolean=false):void{
         this.app.get(path, async (req, res)=> {
             const readResults = await read(filepath);
             const toRun:Array<Function> = [];
@@ -55,12 +55,13 @@ class Translucid {
                 });
             }
             decorated.push((prev:any):void => {
-                // console.log(`${__dirname}/../../${filepath}`);
-                res.contentType(`${__dirname}/../../${filepath}`);
-                res.send(prev);
+                if (!sendFiles)
+                    res.send(prev);
+                else
+                    res.sendFile(prev);
             });
 
-            decorated[0](readResults);
+            decorated[0](sendfiles?readResults:`${__dirname}/../../${filepath}`);
         });
     }
 }
