4fb90beecb077c2e0c271ea81cfad6434c60272b
Matei Adriel <rafaeladriel11@gmail.com>
2019-04-10 21:37:48 +0300

    typescript(simio-client): my second attempt at trying to deploy to gh pages
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/simio-client/src/environments/environment.prod.ts
+++ typescript/simio-client/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBMhf35HijKKSRZdRYt_eBNLGadhQKdZVY",
+    authDomain: "planets-io.firebaseapp.com",
+    databaseURL: "https://planets-io.firebaseio.com",
+    projectId: "planets-io",
+    storageBucket: "planets-io.appspot.com",
+    messagingSenderId: "209812924936"
+  }
 };
