dfab3750f9d7d4ce84c33d6db074de758368109e
Mateiadrielrafael <rafaeladriel11@gmail.com>
2019-08-11 12:14:56 +0300

    typescript(lunargame/api): basic jest setup
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/lunargame/api/src/modules/core/router.ts
+++ typescript/lunargame/api/src/modules/core/router.ts
@@ -1,14 +1,7 @@
-import { router as accountRouter } from '../auth/routes/account'
-import { router as authRouter } from '../auth/routes/auth'
-import { router as userRouter } from '../auth/routes/user'
-import { router as gameRouter } from '../game/routes/game'
 import Router from 'koa-router'
 
 const router = new Router()
 
-router.use('/account', accountRouter.middleware())
-router.use('/auth', authRouter.middleware())
-router.use('/user', userRouter.middleware())
-router.use('/game', gameRouter.middleware())
+router.use('/game')
 
 export { router }
