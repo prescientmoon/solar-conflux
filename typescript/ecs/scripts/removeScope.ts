e967ff0016659e670107b245066801fc400954b3
Matei Adriel <rafaeladriel11@gmail.com>
2019-11-18 22:47:33 +0200

    typescript(ecs): now the script also changes the registry
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/ecs/scripts/removeScope.ts
+++ typescript/ecs/scripts/removeScope.ts
@@ -5,9 +5,16 @@ const path = resolve(__dirname, '../package.json')
 
 const npmConfig = JSON.parse(readFileSync(path).toString()) as {
   name: string
+  publishConfig: {
+    registry: string
+  }
 }
 
 npmConfig.name =
   '@Mateiadrielrafael' + npmConfig.name.substr(npmConfig.name.indexOf('/'))
+
+npmConfig.publishConfig = {
+  registry: 'https://npm.pkg.github.com/'
+}
 
 writeFileSync(path, JSON.stringify(npmConfig))
