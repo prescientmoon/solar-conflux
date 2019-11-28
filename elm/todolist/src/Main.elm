8cd4ef4ab8cd7b500dad1c951ab1423908b9cb3d
Matei Adriel <rafaeladriel11@gmail.com>
2019-11-28 16:57:45 +0200

    elm(todolist): feat: basic todo operations
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- elm/todolist/src/Main.elm
+++ elm/todolist/src/Main.elm
@@ -1,20 +1,36 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+
+
+-- HELPER TYPES
+
+
+type alias Todo =
+    { done : Bool
+    , text : String
+    , id : Int
+    }
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { todos : List Todo
+    , lastId : Int
+    , nextTodoText : String
+    }
 
 
-init : ( Model, Cmd Msg )
+init : Model
 init =
-    ( {}, Cmd.none )
+    Model [] 0 ""
 
 
 
@@ -22,12 +38,48 @@ init =
 
 
 type Msg
-    = NoOp
+    = AddTodo
+    | DeleteTodo Int
+    | ToggleTodoCompletion Int
+    | SetNextTodoText String
 
 
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    ( model, Cmd.none )
+update : Msg -> Model -> Model
+update message model =
+    case message of
+        AddTodo ->
+            if model.nextTodoText == "" then
+                model
+
+            else
+                let
+                    id =
+                        model.lastId + 1
+
+                    text =
+                        model.nextTodoText
+                in
+                { model | lastId = id, todos = Todo False text id :: model.todos }
+
+        DeleteTodo id ->
+            { model | todos = List.filter ((/=) id << .id) model.todos }
+
+        ToggleTodoCompletion id ->
+            { model
+                | todos =
+                    List.map
+                        (\todo ->
+                            if todo.id == id then
+                                { todo | done = not todo.done }
+
+                            else
+                                todo
+                        )
+                        model.todos
+            }
+
+        SetNextTodoText text ->
+            { model | nextTodoText = text }
 
 
 
@@ -36,9 +88,69 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    div [ class "container" ]
+        [ header model
+        , div [ class "todos" ] <|
+            List.map
+                todoView
+                model.todos
+        ]
+
+
+
+-- HEADER
+
+
+header : Model -> Html Msg
+header _ =
+    div [ class "header" ]
+        [ button
+            [ class "btn"
+            , onClick AddTodo
+            ]
+            [ text "add todo" ]
+        , input
+            [ placeholder "todo text"
+            , onInput SetNextTodoText
+            , class "todoTextInput"
+            ]
+            []
+        ]
+
+
+
+-- _TODO VIEW
+
+
+todoClasses : Todo -> String
+todoClasses todo =
+    "todo"
+        ++ (if todo.done then
+                " completed"
+
+            else
+                ""
+           )
+
+
+todoView : Todo -> Html Msg
+todoView todo =
+    div [ class <| todoClasses todo ]
+        [ div [ class "todoCompleted" ]
+            [ input
+                [ checked todo.done
+                , type_ "checkbox"
+                , class "todoCheckbox"
+                , onCheck <| \_ -> ToggleTodoCompletion todo.id
+                ]
+                []
+            ]
+        , div [ class "todoText" ] [ text todo.text ]
+        , button
+            [ class "btn"
+            , onClick <| DeleteTodo todo.id
+            ]
+            [ text "Delete todo" ]
         ]
 
 
@@ -48,9 +160,8 @@ view model =
 
 main : Program () Model Msg
 main =
-    Browser.element
+    Browser.sandbox
         { view = view
-        , init = \_ -> init
+        , init = init
         , update = update
-        , subscriptions = always Sub.none
         }
