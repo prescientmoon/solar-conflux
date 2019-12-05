1b106c1465da10de897f00bfb8a17c2fcdc94650
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-05 19:25:03 +0200

    fsharp(ygosim): feat: added a way of creating a new board
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/ygosim/src/Side.fs
+++ fsharp/ygosim/src/Side.fs
@@ -3,8 +3,15 @@ module Side
 open Card
 
 type Side =
-    { field: Card
+    { field: Card option
       monsters: Card list
       spells: Card list
       graveyard: Card list
       deck: Card list }
+
+let emptySide =
+    { field = None
+      monsters = []
+      spells = []
+      graveyard = []
+      deck = [] }
