611fbef524bf56644bff5e1cf251e3ad67e4a424
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-06 11:51:27 +0200

    fsharp(ygosim): chore: grouped the Board, Side and Player modules
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/ygosim/src/Board.fs
+++ fsharp/ygosim/src/Board.fs
@@ -1,8 +1,42 @@
 module Board
 
-open Player
+module Side =
+    open Card
 
-type Board =
-    { players: Player * Player }
+    type Side =
+        { field: Card option
+          monsters: Card list
+          spells: Card list
+          graveyard: Card list
+          deck: Card list }
 
-let emptyBoard = { players = (initialPlayer 8000, initialPlayer 8000) }
+    let emptySide =
+        { field = None
+          monsters = []
+          spells = []
+          graveyard = []
+          deck = [] }
+
+
+module Player =
+    open Side
+
+    type Player =
+        { lifePoints: int
+          side: Side }
+
+    let inflictDamage (player: Player) amount = { player with lifePoints = player.lifePoints - amount }
+
+    let initialPlayer lp =
+        { lifePoints = lp
+          side = emptySide }
+
+
+
+module Board =
+    open Player
+
+    type Board =
+        { players: Player * Player }
+
+    let emptyBoard = { players = (initialPlayer 8000, initialPlayer 8000) }
