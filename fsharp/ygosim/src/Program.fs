eab212c0394bd4592adb619080de1a0cdf6e7850
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-07 19:31:53 +0200

    fsharp(ygosim): feat: added the basic drawing and next-turn stuff
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/ygosim/src/Program.fs
+++ fsharp/ygosim/src/Program.fs
@@ -1,5 +1,20 @@
 ﻿module Main =
+    open Board
+    open Card
+    open Game
+
     [<EntryPoint>]
     let main argv =
-        printfn "Hello World from F#!"
-        0 // return an integer exit code
+        let board = Board.emptyBoard
+        let sampleCard = Spell ({name= "sampleCard"; text="something"}, {spellType = Card.ContinuosSpell})
+
+        let (first, second) = board.players
+        let secondBoard = withCurrentPlayer <| Game.toDeckBottom sampleCard <| board
+
+        printfn "%A" secondBoard
+
+        let thirdBoard = doTurn secondBoard
+
+        printfn "%A" thirdBoard
+
+        0
