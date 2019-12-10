b3f374162e65d7ae52584c48689167179e4ee379
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-10 16:09:06 +0200

    fsharp(ygosim): feat: finally found a good way of doing the client stuff
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/ygosim/src/Program.fs
+++ fsharp/ygosim/src/Program.fs
@@ -9,35 +9,13 @@
         let sampleCard = Card.Spell ({name= "sampleCard"; text="something"; effects = []}, {spellType = Card.ContinuosSpell})
         let board = over Board.currentPlayer <| toDeckBottom sampleCard <| emptyBoard
 
-        let consoleClient command = 
-            match command with
-            | Log message -> 
-                printfn "%s" message
-                NoResult
-            // | _ -> NoResult
+        let client action =
+            match action with
+            | StateChanged newState -> printfn "The new state you got is: %A" newState
+            | NewPhase phase -> printfn "New phse: %A" phase
+            | _ -> printfn "Something unkown happened"
 
-        let parseAction _ = 
-            printf "What action do you want to do?"
+            0
 
-            let stringified = System.Console.ReadLine()
-
-            match stringified with
-            | "draw" -> InitialDraw
-            | "pass" -> Pass
-            | _ -> failwith "unknown command"
- 
-
-
-        let rec loop board = 
-            let action = parseAction
-
-            let (newBoard, success) = processAction consoleClient board <| board^. Board.currentPlayer <| action()
-
-            if success then
-                printfn "%A" newBoard
-            printfn "%b" success
-
-            loop newBoard
-
-        loop board
+        game board client
         0
