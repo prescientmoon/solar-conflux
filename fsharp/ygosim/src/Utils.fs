e6dc6097f9314a65c543dfadd3217b0456e315ca
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-15 15:20:20 +0200

    fsharp(ygosim): feat: added a helper to create a lens from an array index
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/ygosim/src/Utils.fs
+++ fsharp/ygosim/src/Utils.fs
@@ -1,7 +1,15 @@
 module Utils
 
 module List =
-    let setIndex index value list =
-        List.mapi (fun previous i ->
+    let setIndex index value =
+        List.mapi (fun i previous ->
             if i = index then value
             else previous)
+
+
+    let (.->) = setIndex
+
+module Lens =
+    open FSharpPlus.Lens
+
+    let inline indexToLens index f (array: list<'a>) = f array.[index] <&> fun v -> List.setIndex index v array
