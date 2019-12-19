4cdd3862953017c414c970ae4595b17e0ff08f63
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-19 14:01:33 +0200

    typescript(option): chore: replaced rollup-plugin-node-resolve with @rollup/plugin-node-resolve
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/option/rollup.config.ts
+++ typescript/option/rollup.config.ts
@@ -1,5 +1,5 @@
 import commonjs from 'rollup-plugin-commonjs'
-import nodeResolve from 'rollup-plugin-node-resolve'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import dts from 'rollup-plugin-dts'
 import typescript from 'rollup-plugin-typescript2'
 import { terser } from 'rollup-plugin-terser'
