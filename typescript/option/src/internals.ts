906096a5cb5d60eb98e477fa8a56fc54030bb417
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-22 15:38:16 +0200

    typescript(option): feat: Refactored everything to use nominal types typescript(option): typescript(option): BREAKING CHANGE: flat only works on one layer but is now strongly typed | everything uses nominal types now | match has the correct parameter order
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/option/src/internals.ts
+++ typescript/option/src/internals.ts
@@ -1,25 +1,7 @@
-export const isOption = Symbol('option')
-export const someValue = Symbol('value')
+export const some = Symbol('some')
+export const none = Symbol('none')
 
-export class SomeClass<T> {
-    public [isOption] = true
-    public [someValue]: T
-
-    public constructor(value: T) {
-        this[someValue] = value
-    }
-
-    public toString() {
-        return `Some(${this[someValue]})`
-    }
+export type NominalTyped<T, U> = {
+    type: T
+    value: U
 }
-
-export class NoneClass {
-    public [isOption] = true
-
-    public toString() {
-        return 'None'
-    }
-}
-
-export default { NoneClass, SomeClass, isOption, someValue }
