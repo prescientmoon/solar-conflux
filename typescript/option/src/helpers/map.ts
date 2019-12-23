6dc7e99eb60e0faa15fa60319d2e6a065d7d27e3
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-23 15:17:09 +0200

    typescript(option): fix: compL was only used once so it wasn't worth the bundle sacrifice
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/option/src/helpers/map.ts
+++ typescript/option/src/helpers/map.ts
@@ -1,11 +1,10 @@
 import { match } from './match'
 import { Mapper } from '../internalTypes'
 import { Option, Some, None } from '../types'
-import { compL } from '@thi.ng/compose'
 
 export const map = <T, U>(
     mapper: Mapper<T, U>,
     option: Option<T>
 ): Option<U> => {
-    return match(compL(mapper, Some), None, option)
+    return match(v => Some(mapper(v)), None, option)
 }
