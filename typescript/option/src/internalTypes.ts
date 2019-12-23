c652e1a649112e5c1c508493295c86635d8ec335
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-23 15:00:29 +0200

    typescript(option): fix: fixed wrong types for bind, bindAsync, and probably other helpers as well
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/option/src/internalTypes.ts
+++ typescript/option/src/internalTypes.ts
@@ -1,8 +1,8 @@
 import { Option } from './types'
 
 export type Mapper<T, U> = (v: T) => U
-export type Binder<T, U> = Mapper<T, Option<U>>
-export type Predicate<T> = Mapper<T, boolean>
+export type Binder<T, U> = (v: T) => Option<U>
+export type Predicate<T> = (v: T) => boolean
 export type Folder<T, U> = (s: U, v: T) => U
 export type BackFolder<T, U> = (v: T, s: U) => U
 export type Nullable<T> = T | null
