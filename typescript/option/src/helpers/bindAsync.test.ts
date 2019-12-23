1a443ee3492a5fd8db4e8e7e9623df78b5215ad7
Matei Adriel <rafaeladriel11@gmail.com>
2019-12-23 16:01:51 +0200

    typescript(option): test: added tests for bindAsync
    Signed-off-by: prescientmoon <git@moonythm.dev>

---
+++ typescript/option/src/helpers/bindAsync.test.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai'
+import { Some, None } from '../types'
+import { constantly } from '@thi.ng/compose'
+import { bindAsync } from './bindAsync'
+
+describe('The bindAsync helper', () => {
+    it('should return None for any callback when given None', async () => {
+        // act
+        const result = await bindAsync(async v => Some(v), None)
+
+        // assert
+        expect(result).to.equal(None)
+    })
+
+    describe('When given Some', () => {
+        it('should return None if the callback returns None', async () => {
+            // act
+            const result = await bindAsync(async _ => None, Some(3))
+
+            // assert
+            expect(result).to.equal(None)
+        })
+
+        it('should return Some if the callback returns Some', async () => {
+            // act
+            const result = await bindAsync(async x => Some(x + 1), Some(3))
+
+            // assert
+            expect(result).to.equal(Some(4))
+        })
+    })
+})
