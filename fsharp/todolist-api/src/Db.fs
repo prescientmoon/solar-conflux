bb2da06206dd8068f3cb03550b0fdaeea748116e
Matei Adriel <rafaeladriel11@gmail.com>
2020-01-02 13:48:35 +0200

    fsharp(todolist-api): feat: query for getting todos by id
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/todolist-api/src/Db.fs
+++ fsharp/todolist-api/src/Db.fs
@@ -7,6 +7,26 @@ let ConnectionString = "Server=127.0.0.1; Database=todo_api_db; User Id=suave; P
 
 type Sql = SqlDataProvider<ConnectionString=ConnectionString, DatabaseVendor=Common.DatabaseProviderTypes.POSTGRESQL, CaseSensitivityChange=Common.CaseSensitivityChange.ORIGINAL>
 
-type DbContext = Sql.dataContext
+module Context =
+    type DbContext = Sql.dataContext
 
-type Todo = DbContext.``public.todosEntity``
+    let getContext() = Sql.GetDataContext()
+
+
+module Types =
+    open Context
+
+    type Todo = DbContext.``public.todosEntity``
+
+module Queries =
+    open Context
+    open Types
+
+    let getTodosById id (ctx: DbContext): Todo option =
+        query {
+            for todo in ctx.Public.Todos do
+                where (todo.Id = id)
+                select todo
+        }
+        |> Seq.tryHead
+ 
