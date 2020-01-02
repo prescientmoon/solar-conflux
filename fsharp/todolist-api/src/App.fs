7234b642b8fa7b5900c64cb5fae789198be7d1b8
Matei Adriel <rafaeladriel11@gmail.com>
2020-01-02 13:49:26 +0200

    fsharp(todolist-api): feat: route for the todo by id thing
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- fsharp/todolist-api/src/App.fs
+++ fsharp/todolist-api/src/App.fs
@@ -2,10 +2,25 @@
 
 open Suave
 open Suave.Successful
+open Suave.Operators
+open Suave.Filters
+open Suave.RequestErrors
+
+module App =
+    let todoById (id) = 
+        let todo = Db.Context.getContext() |> Db.Queries.getTodosById id
+
+        match todo with 
+        | Some inner -> OK <| sprintf "%A" inner.Name
+        | None -> id |> sprintf "Cannot find todo with id %i" |> NOT_FOUND 
+
+    let mainWebPart: WebPart = choose [
+        GET >=> choose [
+            pathScan "/todos/%i" todoById
+        ]]
 
 [<EntryPoint>]
-let main argv =
-    
-    startWebServer defaultConfig (OK "hello world")
+let main _ =
+    startWebServer defaultConfig App.mainWebPart
 
     0 // return an integer exit code
