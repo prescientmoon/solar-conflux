658b59e9e65c6e5f429c6e2e59ee59e42b32080e
Matei Adriel <rafaeladriel11@gmail.com>
2020-05-10 19:58:18 +0300

    typescript(monorepo-template): chore: removed debug logs from template
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- typescript/monorepo-template/_templates/package/new/index.js
+++ typescript/monorepo-template/_templates/package/new/index.js
@@ -1,11 +1,9 @@
-const packageJson = require("../../../package.json");
-
-console.log(JSON.stringify(packageJson.repository));
+const packageJson = require('../../../package.json')
 
 exports.prompt = async ({ args }) => ({
   ...packageJson,
   ...args,
   org: packageJson.name,
   repository: JSON.stringify(packageJson.repository),
-  bugs: JSON.stringify(packageJson.bugs),
-});
+  bugs: JSON.stringify(packageJson.bugs)
+})
