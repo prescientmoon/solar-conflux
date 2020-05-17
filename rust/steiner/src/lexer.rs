18f55d8fef502ceef465e5daaf91307132eeaff4
Matei Adriel <rafaeladriel11@gmail.com>
2020-05-17 17:44:23 +0300

    rust(steiner): feat: do not allow identifiers to overlap with keywords
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- rust/steiner/src/lexer.rs
+++ rust/steiner/src/lexer.rs
@@ -1,7 +1,7 @@
 use nom::branch::alt;
 use nom::bytes::complete::{is_a, tag};
-use nom::character::complete::{alphanumeric1, multispace0};
-use nom::combinator::{all_consuming, map, value};
+use nom::character::complete::{alphanumeric1, multispace0, space1};
+use nom::combinator::{all_consuming, map, map_opt, value};
 use nom::multi::many0;
 use nom::number::complete::double;
 use nom::sequence::terminated;
@@ -28,6 +28,12 @@ pub enum KeywordKind {
     Else,
 }
 
+macro_rules! keyword {
+    ($tag: expr, $keyword: expr) => {
+        value($keyword, terminated(tag($tag), space1))
+    };
+}
+
 #[derive(Debug)]
 pub enum Token<'a> {
     FloatLit(f64),
@@ -38,6 +44,7 @@ pub enum Token<'a> {
 }
 
 const OPERATOR_CHARS: &[u8] = b"<=>+-/*!$%^&|";
+const KEYWORDS: [&[u8]; 3] = [b"if", b"then", b"else"];
 
 pub fn lex(input: &[u8]) -> IResult<&[u8], Vec<Token>> {
     let parse_float_literal = map(double, Token::FloatLit);
@@ -48,17 +55,24 @@ pub fn lex(input: &[u8]) -> IResult<&[u8], Vec<Token>> {
         )),
         Token::Punctuation,
     );
+
     let parse_keyword = map(
         alt((
-            tagged!(b"if", KeywordKind::If),
-            tagged!(b"then", KeywordKind::Then),
-            tagged!(b"else", KeywordKind::Else),
+            keyword!(KEYWORDS[0], KeywordKind::If),
+            keyword!(KEYWORDS[1], KeywordKind::Then),
+            keyword!(KEYWORDS[2], KeywordKind::Else),
         )),
         Token::Keyword,
     );
 
     let parse_operator = map(is_a(OPERATOR_CHARS), Token::Operator);
-    let parse_identifier = map(alphanumeric1, Token::Identifier);
+    let parse_identifier = map_opt(alphanumeric1, |id| {
+        if KEYWORDS.contains(&id) {
+            None
+        } else {
+            Some(Token::Identifier(id))
+        }
+    });
 
     let parse = all_consuming(many0(terminated(
         alt((
