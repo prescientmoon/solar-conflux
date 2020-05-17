84d7e0d8187ea6c4702cd8c365fb70c29b3254c0
Matei Adriel <rafaeladriel11@gmail.com>
2020-05-17 17:13:58 +0300

    rust(steiner): feat: keywords
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- rust/steiner/src/lexer.rs
+++ rust/steiner/src/lexer.rs
@@ -1,45 +1,70 @@
 use nom::branch::alt;
 use nom::bytes::complete::{is_a, tag};
 use nom::character::complete::{alphanumeric1, multispace0};
-use nom::combinator::{all_consuming, map};
+use nom::combinator::{all_consuming, map, value};
 use nom::multi::many0;
 use nom::number::complete::double;
 use nom::sequence::terminated;
 use nom::IResult;
 use std::vec::Vec;
 
-#[derive(Debug)]
+// This is a simple macro for making parsers which replace a tag with a custom value
+macro_rules! tagged {
+    ($tag:expr,$value:expr) => {
+        value($value, tag($tag))
+    };
+}
+
+#[derive(Debug, Clone)]
 pub enum PunctuationKind {
     OpenParenthesis,
     CloseParenthesis,
 }
 
+#[derive(Debug, Clone)]
+pub enum KeywordKind {
+    If,
+    Then,
+    Else,
+}
+
 #[derive(Debug)]
-pub enum TokenKind<'a> {
+pub enum Token<'a> {
     FloatLit(f64),
     Punctuation(PunctuationKind),
+    Keyword(KeywordKind),
     Operator(&'a [u8]),
     Identifier(&'a [u8]),
 }
 
 const OPERATOR_CHARS: &[u8] = b"<=>+-/*!$%^&|";
 
-pub fn lex(input: &[u8]) -> IResult<&[u8], Vec<TokenKind>> {
-    let parse_float_literal = map(double, TokenKind::FloatLit);
+pub fn lex(input: &[u8]) -> IResult<&[u8], Vec<Token>> {
+    let parse_float_literal = map(double, Token::FloatLit);
     let parse_punctuation = map(
         alt((
-            map(tag(b"("), |_| PunctuationKind::OpenParenthesis),
-            map(tag(b")"), |_| PunctuationKind::CloseParenthesis),
+            tagged!(b"(", PunctuationKind::OpenParenthesis),
+            tagged!(b")", PunctuationKind::CloseParenthesis),
         )),
-        TokenKind::Punctuation,
+        Token::Punctuation,
     );
-    let parse_operator = map(is_a(OPERATOR_CHARS), TokenKind::Operator);
-    let parse_identifier = map(alphanumeric1, TokenKind::Identifier);
+    let parse_keyword = map(
+        alt((
+            tagged!(b"if", KeywordKind::If),
+            tagged!(b"then", KeywordKind::Then),
+            tagged!(b"else", KeywordKind::Else),
+        )),
+        Token::Keyword,
+    );
+
+    let parse_operator = map(is_a(OPERATOR_CHARS), Token::Operator);
+    let parse_identifier = map(alphanumeric1, Token::Identifier);
 
     let parse = all_consuming(many0(terminated(
         alt((
             parse_float_literal,
             parse_punctuation,
+            parse_keyword,
             parse_operator,
             parse_identifier,
         )),
