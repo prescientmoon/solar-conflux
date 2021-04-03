0bb81bfc46f54838613fd9e4b02839aa35ca6d79
Matei Adriel <rafaeladriel11@gmail.com>
2021-04-04 00:37:14 +0300

    feat: basic rendering

--- purescript/factorio-throughput/src/Main.purs
+++ purescript/factorio-throughput/src/Main.purs
@@ -2,39 +2,34 @@ module Main where
 
 import Prelude
 
-import Data.Compactable (compact)
 import Data.Either (Either(..))
-import Data.Foldable (for_)
-import Data.HashMap as HashMap
-import Data.String (joinWith)
 import Data.Tuple (Tuple(..))
 import Data.Tuple.Nested ((/\))
 import Effect (Effect)
-import Effect.Class.Console (logShow)
 import Effect.Console (log)
-import RealFunction (PortSide(..), RealFunction, SolveM, _constraints, collectConstraints, myFactory, runSolveM, tryFindBound)
-import Run.Except (runFail)
-import Run.Reader.Extra (fromState')
+import Moontorio.Render (RenderFn, renderFactory)
+import RealFunction (PortSide(..), RealFunction, SolveM, collectConstraints, myFactory, runSolveM, tryFindBoundSolveM)
 
 p :: SolveM (Array RealFunction)
 p = do
   collectConstraints
-  a <- fromState' _constraints $ runFail $ tryFindBound (0 /\ Input)
-  b <- fromState' _constraints $ runFail $ tryFindBound (0 /\ Output)
-  c <- fromState' _constraints $ runFail $ tryFindBound (1 /\ Input)
-  d <- fromState' _constraints $ runFail $ tryFindBound (1 /\ Output)
-  e <- fromState' _constraints $ runFail $ tryFindBound (2 /\ Input)
-  f <- fromState' _constraints $ runFail $ tryFindBound (2 /\ Output)
-  g <- fromState' _constraints $ runFail $ tryFindBound (4 /\ Input)
-  h <- fromState' _constraints $ runFail $ tryFindBound (4 /\ Output)
-  pure $ compact [a, b, c, d, e, f, g, h]
+  a <- tryFindBoundSolveM (0 /\ Input)
+  b <- tryFindBoundSolveM (0 /\ Output)
+  c <- tryFindBoundSolveM (1 /\ Input)
+  d <- tryFindBoundSolveM (1 /\ Output)
+  e <- tryFindBoundSolveM (2 /\ Input)
+  f <- tryFindBoundSolveM (2 /\ Output)
+  g <- tryFindBoundSolveM (4 /\ Input)
+  h <- tryFindBoundSolveM (4 /\ Output)
+  pure [a, b, c, d, e, f, g, h]
 
-main :: Effect Unit
-main = do
-  for_ (HashMap.toArrayBy Tuple myFactory) \(Tuple key value) -> log $ show key <> ": " <> show value
+main :: RenderFn -> Effect Unit
+main render = do
+  -- for_ (HashMap.toArrayBy Tuple myFactory) \(Tuple key value) -> log $ show key <> ": " <> show value
 
   case runSolveM myFactory p of
     Left err -> log err
     Right (Tuple s f) -> do
-      log $ joinWith "\n" $ show <$> s.constraints
-      logShow $ f <*> pure 0.0
+      renderFactory render myFactory s.constraints
+      -- log $ joinWith "\n" $ show <$> s.constraints
+      -- logShow $ f <*> pure 0.0
