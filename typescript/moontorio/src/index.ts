02196b006785498d7041e9b4598311b757490a0c
Sandu Victor <vyctor661@gmail.com>
2021-05-05 20:22:40 +0300

    camera and player movement changes + window resizing feature + clear function bug fix

--- typescript/moontorio/src/index.ts
+++ typescript/moontorio/src/index.ts
@@ -9,15 +9,15 @@ import * as MoveBeltItems from "./systems/moveBeltItems";
 import { allTiles } from "./utils/traversals";
 import { isBelt, machineIs } from "./utils/machines";
 import {
-  updateLoader,
-  loaderRenderer,
-  loaderItemRenderer,
-  implBeltForLoader,
+    updateLoader,
+    loaderRenderer,
+    loaderItemRenderer,
+    implBeltForLoader,
 } from "./systems/loaders";
 import {
-  implBeltForJunction,
-  renderJunction,
-  updateJunction,
+    implBeltForJunction,
+    renderJunction,
+    updateJunction,
 } from "./systems/junction";
 import { EventEmitter } from "./utils/events";
 import { getBeltLength } from "./systems/beltCurving";
@@ -25,52 +25,53 @@ import { getBeltLength } from "./systems/beltCurving";
 const canvas = document.getElementById("canvas")! as HTMLCanvasElement;
 const ctx = canvas.getContext("2d")!;
 const state: GameState = {
-  ctx,
-  camera: {
-    translation: [0, 0],
-    scale: 20,
-  },
-  keyboard: pressedKeys(),
-  player: {
-    position: [0, 0],
-    rotation: 0,
-  },
-  map: {
-    chunkMap: [
-      [createChunk(), createChunk()],
-      [createChunk(), createChunk()],
-    ],
-  },
-  machineInterfaces: {
-    belt: {
-      beltLike: MoveBeltItems.implBeltForBelt,
+    ctx,
+    camera: {
+        translation: [canvas.width / 2, canvas.height / 2],
+        scale: 20,
     },
-    junction: {
-      beltLike: implBeltForJunction,
+    keyboard: pressedKeys(),
+    player: {
+        position: [0, 0],
+        rotation: 0,
+        speedMultiplier: 3,
     },
-    loader: {
-      beltLike: implBeltForLoader,
+    map: {
+        chunkMap: [
+            [createChunk(), createChunk()],
+            [createChunk(), createChunk()],
+        ],
     },
-  },
-  items,
-  tick: 0,
-  emitter: new EventEmitter(),
+    machineInterfaces: {
+        belt: {
+            beltLike: MoveBeltItems.implBeltForBelt,
+        },
+        junction: {
+            beltLike: implBeltForJunction,
+        },
+        loader: {
+            beltLike: implBeltForLoader,
+        },
+    },
+    items,
+    tick: 0,
+    emitter: new EventEmitter(),
 };
 
 state.emitter.on("machineCreated", (d) => {
-  MoveBeltItems.addBeltLike(state, d.machine, d.position);
-  MoveBeltItems.addBelt(state, d.machine, d.position);
+    MoveBeltItems.addBeltLike(state, d.machine, d.position);
+    MoveBeltItems.addBelt(state, d.machine, d.position);
 });
 
 addMachine(state, [3, 4], {
-  type: "junction",
-  item: item("yellowJunction"),
-  items: [
-    [[], []],
-    [[], []],
-    [[], []],
-    [[], []],
-  ],
+    type: "junction",
+    item: item("yellowJunction"),
+    items: [
+        [[], []],
+        [[], []],
+        [[], []],
+        [[], []],
+    ],
 });
 
 addMachine(state, [3, 3], MoveBeltItems.mkBelt(Direction.Down, "yellowBelt"));
@@ -88,14 +89,14 @@ addMachine(state, [4, 2], MoveBeltItems.mkBelt(Direction.Left, "yellowBelt"));
 addMachine(state, [3, 2], MoveBeltItems.mkBelt(Direction.Left, "yellowBelt"));
 
 addMachine(state, [4, 3], {
-  type: "junction",
-  item: item("yellowJunction"),
-  items: [
-    [[], []],
-    [[], []],
-    [[], []],
-    [[], []],
-  ],
+    type: "junction",
+    item: item("yellowJunction"),
+    items: [
+        [[], []],
+        [[], []],
+        [[], []],
+        [[], []],
+    ],
 });
 
 addMachine(state, [7, 5], MoveBeltItems.mkBelt(Direction.Up, "yellowBelt"));
@@ -108,117 +109,128 @@ addMachine(state, [10, 5], MoveBeltItems.mkBelt(Direction.Down, "yellowBelt"));
 addMachine(state, [10, 6], MoveBeltItems.mkBelt(Direction.Down, "yellowBelt"));
 
 addMachine(state, [10, 7], {
-  type: "loader",
-  direction: Direction.Down,
-  item: item("yellowLoader"),
-  items: [[], []],
+    type: "loader",
+    direction: Direction.Down,
+    item: item("yellowLoader"),
+    items: [[], []],
 });
 
 const testBelts = [
-  state.map.chunkMap[0][0]![3][3],
-  state.map.chunkMap[0][0]![7][5],
-  state.map.chunkMap[0][0]![10][4],
+    state.map.chunkMap[0][0]![3][3],
+    state.map.chunkMap[0][0]![7][5],
+    state.map.chunkMap[0][0]![10][4],
 ] as Belt[];
 
 for (const belt of testBelts) {
-  belt.machine.items[0].push(
-    ...Array(10)
-      .fill(1)
-      .map((_, index) => ({
-        item: item("ironPlate"),
-        position: index * 5,
-      }))
-  );
-  belt.machine.items[1].push(
-    ...Array(10)
-      .fill(1)
-      .map((_, index) => ({
-        item: item("ironPlate"),
-        position: index * 5,
-      }))
-  );
+    belt.machine.items[0].push(
+        ...Array(10)
+            .fill(1)
+            .map((_, index) => ({
+                item: item("ironPlate"),
+                position: index * 5,
+            }))
+    );
+    belt.machine.items[1].push(
+        ...Array(10)
+            .fill(1)
+            .map((_, index) => ({
+                item: item("ironPlate"),
+                position: index * 5,
+            }))
+    );
 }
 
 const testBelt2 = state.map.chunkMap[0][0]![2][2] as Belt;
 
 testBelt2.machine.items[0].push(
-  ...Array(10)
-    .fill(1)
-    .map((_, index) => ({
-      item: item("ironPlate"),
-      position: index * 5,
-    }))
+    ...Array(10)
+        .fill(1)
+        .map((_, index) => ({
+            item: item("ironPlate"),
+            position: index * 5,
+        }))
 );
 testBelt2.machine.items[1].push(
-  ...Array(10)
-    .fill(1)
-    .map((_, index) => ({
-      item: item("ironPlate"),
-      position: index * 5,
-    }))
+    ...Array(10)
+        .fill(1)
+        .map((_, index) => ({
+            item: item("ironPlate"),
+            position: index * 5,
+        }))
 );
 
-console.log(state);
+// console.log(state);
 
 ctx.imageSmoothingEnabled = false;
+const adjustCamera = () => {
+    state.camera.translation[0] = Math.floor(
+        canvas.width / 2 - state.player.position[0]
+    );
+    state.camera.translation[1] = Math.floor(
+        canvas.height / 2 - state.player.position[1]
+    );
+};
 
 const resize = () => {
-  const width = window.innerWidth;
-  const height = window.innerHeight;
+    const width = window.innerWidth;
+    const height = window.innerHeight;
 
-  canvas.width = width;
-  canvas.height = height;
+    canvas.height = height;
+    canvas.width = width;
 
-  canvas.style.width = String(width);
-  canvas.style.height = String(height);
+    canvas.style.width = String(width);
+    canvas.style.height = String(height);
 };
 
 const clear = () => {
-  ctx.clearRect(0, 0, 1000, 1000);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
 
 const main = () => {
-  clear();
+    clear();
 
-  state.tick++;
+    state.tick++;
 
-  // Update stage:
-  updatePlayer(state);
+    // Update stage:
+    updatePlayer(state);
 
-  for (const [tile, position] of allTiles(state)) {
-    if (tile === null) continue;
-    if (isBelt(tile)) MoveBeltItems.updteBelt(state, tile, position);
-    if (machineIs("loader", tile)) updateLoader(state, tile);
-    if (machineIs("junction", tile)) updateJunction(state, tile, position);
-  }
+    for (const [tile, position] of allTiles(state)) {
+        if (tile === null) continue;
+        if (isBelt(tile)) MoveBeltItems.updteBelt(state, tile, position);
+        if (machineIs("loader", tile)) updateLoader(state, tile);
+        if (machineIs("junction", tile)) updateJunction(state, tile, position);
+    }
 
-  // Actual rendering:
-  ctx.translate(state.camera.translation[0], state.camera.translation[1]);
+    // Actual rendering:
+    ctx.translate(state.camera.translation[0], state.camera.translation[1]);
 
-  for (const [tile, position] of allTiles(state)) {
-    if (tile === null) continue;
-    if (isBelt(tile)) beltRenderer(state, tile, position);
-    else if (machineIs("loader", tile)) loaderRenderer(state, tile, position);
-  }
+    for (const [tile, position] of allTiles(state)) {
+        if (tile === null) continue;
+        if (isBelt(tile)) beltRenderer(state, tile, position);
+        else if (machineIs("loader", tile))
+            loaderRenderer(state, tile, position);
+    }
 
-  for (const [tile, position] of allTiles(state)) {
-    if (tile === null) continue;
-    if (isBelt(tile)) beltItemRenderer(state, tile, position);
-    else if (machineIs("loader", tile))
-      loaderItemRenderer(state, tile, position);
-  }
+    for (const [tile, position] of allTiles(state)) {
+        if (tile === null) continue;
+        if (isBelt(tile)) beltItemRenderer(state, tile, position);
+        else if (machineIs("loader", tile))
+            loaderItemRenderer(state, tile, position);
+    }
 
-  for (const [tile, position] of allTiles(state)) {
-    if (tile === null) continue;
-    else if (machineIs("junction", tile)) renderJunction(state, position);
-  }
+    for (const [tile, position] of allTiles(state)) {
+        if (tile === null) continue;
+        else if (machineIs("junction", tile)) renderJunction(state, position);
+    }
 
-  renderPlayer(state);
+    renderPlayer(state);
+    adjustCamera();
 
-  ctx.resetTransform();
+    ctx.resetTransform();
 
-  requestAnimationFrame(main);
+    requestAnimationFrame(main);
 };
 
+window.onresize = resize;
 resize();
 main();
--- typescript/moontorio/src/player.ts
+++ typescript/moontorio/src/player.ts
@@ -1,50 +1,55 @@
 import { GameState, loadAsset } from "./gameState";
 
 export interface Player {
-  position: [number, number];
-  rotation: number;
+    position: [number, number];
+    rotation: number;
+    speedMultiplier: number;
 }
 
 const playerTexture = loadAsset("assets/player.png");
 
 export const renderPlayer = (state: GameState) => {
-  const player = state.player;
+    const player = state.player;
 
-  state.ctx.save();
-  state.ctx.translate(player.position[0] + 20, player.position[1] + 20);
-  state.ctx.rotate(state.player.rotation);
+    state.ctx.save();
+    state.ctx.translate(player.position[0], player.position[1]);
+    state.ctx.rotate(state.player.rotation);
 
-  state.ctx.drawImage(
-    playerTexture,
-    0,
-    0,
-    playerTexture.naturalWidth,
-    playerTexture.naturalHeight,
-    -20,
-    -20,
-    40,
-    40
-  );
+    state.ctx.drawImage(
+        playerTexture,
+        0,
+        0,
+        playerTexture.naturalWidth,
+        playerTexture.naturalHeight,
+        -20,
+        -20,
+        40,
+        40
+    );
 
-  state.ctx.restore();
+    state.ctx.restore();
 };
 
 export const updatePlayer = (state: GameState) => {
-  if (state.keyboard.pressed.has("w")) {
-    state.player.position[0] += Math.cos(state.player.rotation);
-    state.player.position[1] += Math.sin(state.player.rotation);
-  }
+    if (state.keyboard.pressed.has("w")) {
+        state.player.position[0] +=
+            Math.cos(state.player.rotation) * state.player.speedMultiplier;
+        state.player.position[1] +=
+            Math.sin(state.player.rotation) * state.player.speedMultiplier;
+    }
 
-  if (state.keyboard.pressed.has("s")) {
-    state.player.position[0] -= Math.cos(state.player.rotation);
-    state.player.position[1] -= Math.sin(state.player.rotation);
-  }
+    if (state.keyboard.pressed.has("s")) {
+        state.player.position[0] -=
+            Math.cos(state.player.rotation) * state.player.speedMultiplier;
+        state.player.position[1] -=
+            Math.sin(state.player.rotation) * state.player.speedMultiplier;
+    }
 
-  if (state.keyboard.pressed.has("a")) {
-    state.player.rotation -= 0.1;
-  }
+    if (state.keyboard.pressed.has("a")) {
+        state.player.rotation -= 0.1;
+    }
 
-  if (state.keyboard.pressed.has("d")) {
-    state.player.rotation += 0.1;
-  }
+    if (state.keyboard.pressed.has("d")) {
+        state.player.rotation += 0.1;
+    }
 };
