2edcdf5e40a1c5cf8599b62cde8aff2d6d671db2
Sandu Victor <vyctor661@gmail.com>
2021-05-07 19:02:41 +0300

    tile order fixes

--- typescript/moontorio/src/utils/traversals.ts
+++ typescript/moontorio/src/utils/traversals.ts
@@ -1,56 +1,61 @@
 import { GameState, Tile } from "../gameState";
 import { chunkSize } from "../map";
 import { tileAt } from "../systems/world";
+import { array } from "./iterate";
 import { Vec2 } from "./types";
 
-export const allTiles = function* (
-    state: GameState
-): Generator<[Tile | null, Vec2]> {
+const getBigestLength = (lengths: number[]) => {
+    return lengths.sort((a, b) => a - b)[lengths.length - 1];
+};
+
+export const allTiles = function* (state: GameState): Generator<[Tile | null]> {
+    const BiggestLengthOfChunks = getBigestLength([
+        state.map.chunkMap[1][1].length,
+        state.map.chunkMap[0][1].length,
+        state.map.chunkMap[1][0].length,
+        state.map.chunkMap[0][0].length,
+    ]);
+    // console.log(BiggestLengthOfChunks);
+
     for (
-        let directionalMatrixX = 0;
-        directionalMatrixX < state.map.chunkMap.length;
-        directionalMatrixX++
+        let chunkX = -1 * BiggestLengthOfChunks;
+        chunkX < BiggestLengthOfChunks;
+        chunkX++
     )
         for (
-            let directionalMatrixY = 0;
-            directionalMatrixY < state.map.chunkMap.length;
-            directionalMatrixY++
-        )
-            for (
-                let chunkX = 0;
-                chunkX <
-                state.map.chunkMap[directionalMatrixX][directionalMatrixY]
-                    .length;
-                chunkX++
-            )
-                for (
-                    let chunkY = 0;
-                    chunkY <
-                    state.map.chunkMap[directionalMatrixX][directionalMatrixY]
-                        .length;
-                    chunkY++
-                ) {
-                    const chunk =
-                        state.map.chunkMap[directionalMatrixX][
-                            directionalMatrixY
-                        ][chunkX][chunkY];
+            let chunkY = -1 * BiggestLengthOfChunks;
+            chunkY < BiggestLengthOfChunks;
+            chunkY++
+        ) {
+            const chunk =
+                state.map.chunkMap[chunkX >= 0 ? 0 : 1][chunkY >= 0 ? 0 : 1]?.[
+                    Math.abs(chunkX)
+                ]?.[Math.abs(chunkY)];
+            if (!chunk) continue;
 
-                    // console.log(tileAt(state, [0, 0]));
-
-                    if (!chunk) continue;
-
-                    for (let tileX = 0; tileX < chunkSize; tileX++) {
-                        for (let tileY = 0; tileY < chunkSize; tileY++) {
-                            const tile = chunk[tileX][tileY];
-
-                            yield [
-                                tile,
-                                [
-                                    chunkX * chunkSize + tileX,
-                                    chunkY * chunkSize + tileY,
-                                ],
-                            ];
-                        }
+            if (chunkX >= 0 && chunkY >= 0)
+                for (let tileX = 0; tileX < chunkSize; tileX++) {
+                    for (let tileY = 0; tileY < chunkSize; tileY++) {
+                        yield [chunk[tileX][tileY]];
                     }
                 }
+            else if (chunkX < 0 && chunkY >= 0)
+                for (let tileX = chunkSize - 1; tileX >= 0; tileX--) {
+                    for (let tileY = 0; tileY < chunkSize; tileY++) {
+                        yield [chunk[tileX][tileY]];
+                    }
+                }
+            else if (chunkX >= 0 && chunkY < 0)
+                for (let tileX = 0; tileX < chunkSize; tileX++) {
+                    for (let tileY = chunkSize - 1; tileY >= 0; tileY--) {
+                        yield [chunk[tileX][tileY]];
+                    }
+                }
+            else if (chunkX < 0 && chunkY < 0)
+                for (let tileX = chunkSize - 1; tileX >= 0; tileX--) {
+                    for (let tileY = chunkSize - 1; tileY >= 0; tileY--) {
+                        yield [chunk[tileX][tileY]];
+                    }
+                }
+        }
 };
