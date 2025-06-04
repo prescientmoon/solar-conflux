76e72f2b2fcb3a85e129fc953b014fda0e1d45ef
prescientmoon <git@moonythm.dev>
2025-06-04 06:56:02 +0200

    odin(sdl-opengl-rendering): implement outlines for rounded lines
    Signed-off-by: prescientmoon <git@moonythm.dev>

--- odin/sdl-opengl-rendering/src/shaders/vert.glsl
+++ odin/sdl-opengl-rendering/src/shaders/vert.glsl
@@ -1,27 +1,27 @@
 #version 430
 
-layout (location = 0) in vec2 aPos;
-layout (location = 1) in vec4 instanceFill;
-layout (location = 2) in mat3 instanceMatrix;
+layout (location = 0) in vec2  aPos;
+layout (location = 1) in vec4  iFill;
+layout (location = 2) in vec4  iStroke;
+layout (location = 3) in float iStrokeThickness;
+layout (location = 4) in mat3  iMatrix;
 
-out vec4 vertexColor;
-out vec2 vertexPos;
+out vec4  vFill;
+out vec4  vStroke;
+out float vStrokeThickness;
+out vec2  vPos;
 
 layout(std140, binding = 0) uniform Globals {
   mat4 viewportMatrix;
   float aaWidth; // Anti aliasing width
 };
 
-  // struct Line {
-  //   vec2 from;
-  //   vec2 to;
-  // };
-  //
-  // layout (location = 5) in Line instanceLine;
-
 void main() {
-  vec4 pos    = viewportMatrix * vec4(instanceMatrix * vec3(aPos, 1), 1);
+  vec4 pos    = viewportMatrix * vec4(iMatrix * vec3(aPos, 1), 1);
   gl_Position = vec4(pos.xyz, 1);
-  vertexColor = instanceFill;
-  vertexPos = aPos;
+
+  vPos             = aPos.xy;
+  vFill            = iFill;
+  vStroke          = iStroke;
+  vStrokeThickness = iStrokeThickness;
 }
