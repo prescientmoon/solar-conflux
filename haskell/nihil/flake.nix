c0df03a21d14052d2bc4f98cd9294c471dd84a34
prescientmoon <git@moonythm.dev>
2025-06-28 06:05:16 +0200

    haskell(nihil): Init package
    Signed-off-by: prescientmoon <git@moonythm.dev>

---
+++ haskell/nihil/flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, self }:
+    let
+      hpkgs = pkgs: pkgs.haskell.packages."ghc9101";
+      # nihil = pkgs: pkgs.haskell.packages."ghc9101".callPackage ./nihil.nix { };
+      nihil =
+        pkgs:
+        (hpkgs pkgs).developPackage {
+          root = ./.;
+        };
+
+      shell =
+        pkgs:
+        (hpkgs pkgs).developPackage {
+          root = ./.;
+          modifier =
+            drv:
+            pkgs.haskell.lib.addBuildTools drv (
+              with (hpkgs pkgs);
+              [
+                cabal-install
+                ghcid
+                fourmolu
+                haskell-language-server
+              ]
+            );
+        };
+    in
+    {
+      packages."x86_64-linux".nihil = nihil nixpkgs.legacyPackages."x86_64-linux";
+      packages."x86_64-linux".default = self.packages."x86_64-linux".nihil;
+      devShells."x86_64-linux".nihil = shell nixpkgs.legacyPackages."x86_64-linux";
+      devShells."x86_64-linux".default = self.devShells."x86_64-linux".nihil;
+    };
+}
