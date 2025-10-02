30579a0672c878fed3e1cc7517c751df150ae6f2
prescientmoon <git@moonythm.dev>
2025-10-03 01:52:26 +0200

    meta: meta: rust(shaderfurl): Start working on the error tolerant parser :3 33

--- rust/shaderfurl/src/main.rs
+++ rust/shaderfurl/src/main.rs
@@ -1,6 +1,11 @@
-use std::io;
+use std::{io, path::PathBuf, rc::Rc, str::FromStr};
 
-use crate::lexer::{Lexer, TokenKind};
+use ariadne::Source;
+
+use crate::{
+	lexer::{FileId, Lexer, TokenKind},
+	parser::Parser,
+};
 
 mod ast;
 mod cst;
@@ -13,19 +18,27 @@ fn main() {
 	let stdin = io::stdin();
 	stdin.read_line(&mut buffer).unwrap();
 
-	let mut lexer = Lexer::new(buffer);
+	let file_id = FileId::new(Rc::from(PathBuf::from_str("repl").unwrap().as_path()));
+	let mut lexer = Lexer::new(file_id.clone(), &buffer);
 
 	loop {
-		let token = lexer.pull();
+		let token = lexer.next();
 
-		println!(
-			"{:?}: {}",
-			token.kind,
-			&lexer.source[token.span.from.index..token.span.from.index + token.span.length]
-		);
+		println!("{:?}: {}", token.kind, lexer.source_span(&token.span));
 
 		if matches!(token.kind, TokenKind::Eof) {
 			break;
 		}
+	}
+
+	let mut parser = Parser::new(file_id.clone(), &buffer);
+	let expr = parser.parse_expr_call();
+
+	println!("Parse result: {:?}", expr);
+
+	for report in parser.reports() {
+		report
+			.eprint((file_id.clone(), Source::from(&buffer)))
+			.expect("Failed to print report to console ;-;");
 	}
 }
